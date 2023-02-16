890bc0a85a9739b43b6e4d3139ae0c4ea40e60cc
Huy0110 <duchuy01102001@gmail.com>
2023-02-16 23:39:58 +0700

    Add task

--- src/App.js
+++ src/App.js
@@ -2,15 +2,37 @@
 import { useState } from "react";
 function App() {
   const [work, setWork] = useState('')
+  const [toDos, setToDos] = useState([])
+
+  const handleCLick = () => {
+    setToDos(prev => [...prev, work])
+    setWork('')
+  }
   // console.log(work)
   return (
-    <div className="flex gap-8 h-screen justify-center border-red-500 items-center">
-      <input type="text" className="outline-none border border-blue-600 px-4 py-2 w-[400px]" value={work} onChange={e => setWork(e.target.value)}>
-      </input>
+    <div className="flex flex-col gap-8 h-screen justify-center border-red-500 items-center">
+      <div className="flex gap-8">
+        <input type="text" className="outline-none border border-blue-600 px-4 py-2 w-[400px]" value={work} onChange={e => setWork(e.target.value)}>
+        </input>
 
-      <button type="buton" className="outline-none px-4 py-2 bg-blue-500 rounded-md text-white">
-        Add
-      </button>
+        <button type="buton" className="outline-none px-4 py-2 bg-blue-500 rounded-md text-white" onClick={handleCLick}>
+          Add
+        </button>
+      </div>
+      <h3 className="font-bold text-lg">Content</h3>
+      <ul>
+        {
+          toDos.map((toDo, index) => {
+            return (
+              <li key={index}>
+                {toDo}
+              </li>
+            )
+            
+          })
+        }
+      </ul>
+      
     </div>
   );
 }
